d3c03cef5c32abf53820bb0ad037c35b9b45cf65
Haridas250503 <haridasshinde314@gmail.com>
2025-12-31 01:50:40 +0530

    added the files

--- src/main/java/com/dao/StudentDao.java
+++ src/main/java/com/dao/StudentDao.java
@@ -1,10 +1,17 @@
 package com.dao;
 
+import org.springframework.jdbc.core.JdbcTemplate;
+
 import com.model.Student;
+
+
+
 
 public class StudentDao {
 
+	private JdbcTemplate j;
 	public int insertStduent(Student s) {
 		
+		return 0;
 	}
 }
