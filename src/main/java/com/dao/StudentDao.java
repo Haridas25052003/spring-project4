71b74f79cb942f225b5fc2987c53be798e2e85c9
Haridas250503 <haridasshinde314@gmail.com>
2026-01-01 01:39:28 +0530

    performed the crud operations

--- src/main/java/com/dao/StudentDao.java
+++ src/main/java/com/dao/StudentDao.java
@@ -1,10 +1,14 @@
 package com.dao;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import com.model.Student;
+import com.util.StudentRowMapper;
+
 
 
 
@@ -23,5 +27,36 @@ public class StudentDao {
 		String sql="insert into student (name,city,percentage) values(?,?,?)";
 		return j.update(sql,new Object[] {s.getName(),s.getCity(),s.getPercentage()});
 			
+	}
+	
+	public int deleteStudentById(int id) {
+		
+		String sql="delete from student where id=?";
+		return j.update(sql,id);
+	}
+	
+	public Student findStudentById(int id) {
+		
+		String sql="select id,name,city,percentage from student where id=?";
+		List <Student> list=j.query(sql, new StudentRowMapper(),id);
+		return (!list.isEmpty())?list.get(0):null;
+	}
+	
+	public Student findStudentByName(String name) {
+		
+		String sql="select id,name,city,percentage from student where name=?";
+		List <Student> list=j.query(sql, new StudentRowMapper(),name);
+		return (!list.isEmpty())?list.get(0):null;
+	}
+	
+	public List<Student> findAllStudent(){
+		
+		String sql="select id ,name,city,percentage from student";
+		return j.query(sql,new StudentRowMapper());
+	}
+	
+	public int updateStudent(Student s) {
+		String sql="update student set name=?,city=?,percentage=? where id=?";
+		return j.update(sql,new Object[] {s.getName(),s.getCity(),s.getPercentage(),s.getId()});
 	}
 }
--- src/main/java/spring_project4/spring_project4/App.java
+++ src/main/java/spring_project4/spring_project4/App.java
@@ -11,20 +11,34 @@ import com.model.Student;
  * Hello world!
  *
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        ApplicationContext context=new AnnotationConfigApplicationContext(MyConfiguration.class);
-        StudentDao sd=context.getBean(StudentDao.class);
-        
-        Student s=new Student();
-        
-        s.setName("David Warner");
-        s.setCity("Austrelia");
-        s.setPercentage(95.50);
-        System.out.println(sd.insertStduent(s));
-        
-        
-    }
+public class App {
+	public static void main(String[] args) {
+		ApplicationContext context = new AnnotationConfigApplicationContext(MyConfiguration.class);
+		StudentDao sd = context.getBean(StudentDao.class);
+
+		//Student s = new Student();
+
+//		<------inserting the student in table-------->
+//		s.setName("David Warner");
+//		s.setCity("Austrelia");
+//		s.setPercentage(95.50);
+		
+//		<---------finding the student by id---------->
+//		System.out.println(sd.findStudentById(2));
+		
+//      <----display all the student by ------>
+//	    System.out.println(sd.findAllStudent());
+		
+		
+//      <-----update student by using id------>
+//		Student s=sd.findStudentById(2);
+//		s.setName("wwwwwxxx");
+//	    System.out.println(sd.updateStudent(s));
+		
+		
+//		<----finding the student by name------>
+		Student s=sd.findStudentByName("David Warner");
+		System.out.println(s);
+
+ }
 }
